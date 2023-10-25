9986ff0b81cb24fe60b90349d46ec10488eeb716
znycat <znycat@QQ.com>
2023-10-25 16:21:57 +0800

    peek

--- algo-study/linked-list/src/linked_list_second.rs
+++ algo-study/linked-list/src/linked_list_second.rs
@@ -26,6 +26,14 @@ impl<T> List<T> {
             }
         }
     }
+
+    pub fn peek(&self) -> Option<&T> {
+        // 让 map 作用在引用上，而不是直接作用在 self.head 上，为此我们可以使用 Option 的 as_ref 方法
+        self.head.as_ref().map(|node| &node.elem)
+    }
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -96,5 +104,22 @@ mod test {
         let mut list = List::new();
         list.push(3);
         println!("list: {:?}", list);
+    }
+
+    #[test]
+    fn test_peek() {
+        let mut list: List<i32> = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+        list.peek_mut().map(|value: &mut i32| *value = 42);
+
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
     }
 }
