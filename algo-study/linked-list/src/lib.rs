41a28cabd171e245da3b5a162dbdbb2c144dae10
znycat <znycat@QQ.com>
2023-10-26 16:13:02 +0800

    rc list
    rc list

--- algo-study/linked-list/src/lib.rs
+++ algo-study/linked-list/src/lib.rs
@@ -1,3 +1,4 @@
 // pub mod linked_list;
 pub mod linked_list_first;
 pub mod linked_list_second;
+pub mod linked_list_third;
---
+++ algo-study/linked-list/src/linked_list_third.rs
@@ -0,0 +1,119 @@
+// list1 = A -> B -> C -> D
+// list2 = tail(list1) = B -> C -> D
+// list3 = push(list2, X) = X -> B -> C -> D
+//
+// list1 -> A ---+
+//               |
+//               v
+// list2 ------> B -> C -> D
+//               ^
+//               |
+// list3 -> X ---+
+
+use std::rc::Rc;
+
+type Link<T> = Option<Rc<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+pub struct List<T> {
+    head: Link<T>,
+}
+
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+}
+
+impl<T> List<T> {
+    /// prepend 在缓存前面加值；头部追加
+    pub fn prepend(&self, elem: T) -> List<T> {
+        // head.clone() 是Rc::clone 仅仅增加了引用计数，而不是复制
+        let node = Node {
+            elem,
+            next: self.head.clone(),
+        };
+        List {
+            head: Some(Rc::new(node)),
+        }
+    }
+
+    /// 尾部, 除去
+    /// 将现有链表的首个元素移除，并返回剩余的链表
+    pub fn tail(&self) -> List<T> {
+        // let node = self.head.as_ref().map(|node| node.next.clone());
+        // let node = node.and_then(|node| node);
+        // List { head: node }
+        List {
+            head: self.head.as_ref().and_then(|node| node.next.clone()),
+        }
+    }
+
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            next: self.head.as_deref(),
+        }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let list = List::new();
+        assert_eq!(list.head(), None);
+
+        let list = list.prepend(1).prepend(2).prepend(3);
+        assert_eq!(list.head(), Some(&3));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&2));
+
+        let list = list.tail();
+        assert_eq!(list.head(), Some(&1));
+
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+
+        // Make sure empty tail works
+        let list = list.tail();
+        assert_eq!(list.head(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let list = List::new().prepend(1).prepend(2).prepend(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+    }
+}
